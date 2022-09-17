47c7b4a102d0e400071d596330f8954a1d47fd68
Nde Lucien <chelucien08@gmail.com>
2022-09-17 22:38:06 +0100

    made images publicly accessible without need for api key

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const api = process.env.API_URL;
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 app.use(authJwt());
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 
 //===============================  routers ============================
